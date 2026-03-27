38757bfcb9e986c6cef56c7296730fdbffd30f1a
Lobanov <lbnv.mkhl@gmail.com>
2026-03-27 13:57:29 +0500

    Создан запрос для регистрации. Климов

--- APIUserDinner_Klimov/Models/User.cs
+++ APIUserDinner_Klimov/Models/User.cs
@@ -8,7 +8,6 @@ namespace APIUserDinner_Klimov.Models
         public int Id { get; set; }
         public string Login { get; set; }
         public string Email { get; set; }
-        public string PasswordHash { get; set; }
-        public string Token { get; set; }
+        public string Password { get; set; }
     }
 }
